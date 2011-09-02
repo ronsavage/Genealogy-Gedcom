b1509e94e3b8fa6013dc01d7fc28511a7c85396e
Ron Savage <ron@savage.net.au>
2011-09-02 13:36:39 +1000

    Start parsing date ranges

--- lib/Genealogy/Gedcom/Reader/Lexer/Date.pm
+++ lib/Genealogy/Gedcom/Reader/Lexer/Date.pm
@@ -19,8 +19,9 @@ our $VERSION = '0.80';
 sub _init
 {
 	my($self, $arg)  = @_;
-	$$arg{candidate} = $$arg{candidate} || '';   # Caller can set.
-	$$arg{locale}    = $$arg{locale}    || 'en'; # Caller can set.
+	$$arg{candidate} ||= '';     # Caller can set.
+	$$arg{century}   ||= '1900'; # Caller can set.
+	$$arg{locale}    ||= 'en';   # Caller can set.
 	my($user_logger) = defined($$arg{logger}); # Caller can set (e.g. to '').
 	$$arg{logger}    = $user_logger ? $$arg{logger} : Log::Handler -> new;
 	$self            = from_hash($self, $arg);
@@ -65,14 +66,15 @@ sub parse
 
 	my(%date) =
 		(
-		 date   => '',
-		 escape => 'dgregorian',
-		 first  => '',
-		 infix  => '',
-		 last   => '',
-		 locale => $locale,
-		 phrase => '',
-		 prefix => '',
+		 escape          => 'dgregorian',
+		 first           => '',
+		 first_ambiguous => 0,
+		 infix           => '',
+		 last            => '',
+		 last_ambiguous  => 0,
+		 locale          => $locale,
+		 phrase          => '',
+		 prefix          => '',
 		);
 
 	if ($candidate =~ /^(.*)\((.+)\)/)
@@ -144,8 +146,6 @@ sub parse
 		# Expect 'd m y', possibly with abbreviations.
 	}
 
-	$date{date} = join(' ', @field);
-
 	return {%date};
 
 } # End of parse.
@@ -162,7 +162,15 @@ L<Genealogy::Gedcom::Reader::Lexer::Date> - An OS-independent lexer for GEDCOM d
 
 =head1 Synopsis
 
-See L<Genealogy::Gedcom::Reader::Lexer>.
+	my($locale) = 'en';
+	my($parser) = Genealogy::Gedcom::Reader::Lexer::Date -> new
+	(
+	locale => $locale,
+	logger => '',
+	);
+
+	my($candidate)    = 'Bet 4 Apr 2004 and 5 May 2005';
+	my($date_hashref) = $parser -> parse($candidate);
 
 =head1 Description
 
@@ -224,7 +232,7 @@ Default: 1900.
 
 A string which specifies the desired locale.
 
-Only 'en' is supported. However, faint traces of 'nl' (Dutch) are present in the source code (but not in t/date.t).
+Only 'en' is supported. However, some traces of 'nl' (Dutch) are present in the source code (but not in t/date.t).
 
 Default: 'en'.
 
@@ -272,17 +280,27 @@ Get or set the logger object.
 
 To disable logging, just set logger to the empty string.
 
+=head2 parse([%arg])
+
 Here, the [] indicate an optional parameter.
+
+Parse the candidate and return a hashref.
+
+$arg{candidate} => $candiate takes precedence over new(candidate => $candidate).
 
 The string which is a candidate date can be passed in to new as new(candidate => $a_string), or into parse as parse(candidate => $a_string).
 
 This string is always converted to lower case before being processed. Hence all result data is lower case.
 
+The string which is a locale can be passed in to new as new(locale => $a_string), or into parse as parse(locale => $a_string).
+
+$arg{locale} => $locale takes precedence over new(locale => $locale).
+
+Only 'en' is supported.
+
 The return value is a hashref with these key => value pairs:
 
 =over 4
-
-=item o date => $the_date
 
 =item o escape => $the_escape_string
 
@@ -290,9 +308,17 @@ Default: 'dgregorian' (yes, lower case).
 
 =item o first => $first_date_in_range
 
-This is for cases like '1999' in 'Between 1999 and 2000', and '2002' in 'From 2001 to 2002'.
+Returns the first (or only) date.
+
+This is for cases like '1999' in 'about 1999', and for '1999' in 'Between 1999 and 2000', and '2002' in 'From 2001 to 2002'.
 
 Default: ''.
+
+=item o first_ambiguous => $Boolean
+
+Returns 1 if the first (or only) date is ambiguous.
+
+Default: 0.
 
 =item o infix => $a_string
 
@@ -305,6 +331,12 @@ Default: ''.
 This is for cases like '2000' in 'Between 1999 and 2000', and '2002' in 'From 2001 to 2002'.
 
 Default: ''.
+
+=item o last_ambiguous => $Boolean
+
+Returns 1 if the last date is ambiguous.
+
+Default: 0.
 
 =item o locale => $the_user_supplied_locale
 
@@ -326,8 +358,6 @@ Default: ''.
 
 =back
 
-=head2 parse([%arg])
-
 =head1 FAQ
 
 =head1 Machine-Readable Change Log
@@ -338,93 +368,15 @@ The file CHANGES was converted into Changelog.ini by L<Module::Metadata::Changes
 
 Version numbers < 1.00 represent development versions. From 1.00 up, they are production versions.
 
-=head1 References
-
-=over 4
-
-=item o The original Perl L<Gedcom>
-
-=item o GEDCOM
-
-=over 4
-
-=item o L<GEDCOM Specification|http://wiki.webtrees.net/File:Ged551-5.pdf>
-
-=item o L<GEDCOM Validation|http://www.tamurajones.net/GEDCOMValidation.xhtml>
-
-=item o L<GEDCOM Tags|http://www.tamurajones.net/GEDCOMTags.xhtml>
-
-=back
-
-=item o Usage of non-standard tags
-
-=over 4
-
-=item o L<http://www.tamurajones.net/FTWTEXT.xhtml>
-
-This is apparently the worst offender she's seen. Search that page for 'tags'.
-
-=item o L<http://www.tamurajones.net/GenoPro2011.xhtml>
-
-=item o L<http://www.tamurajones.net/GenoPro2007.xhtml>
-
-=item o L<http://www.tamurajones.net/TheFTWTEXTProblem.xhtml>
-
-=back
-
-=item o Other articles on Tamura's site
-
-=over 4
-
-=item o L<http://www.tamurajones.net/FiveFreakyFeaturesYourGenealogySoftwareShouldNotHave.xhtml>
-
-=item o L<http://www.tamurajones.net/TwelveOrdinaryMustHaveGenealogySoftwareFeatures.xhtml>
-
-=back
-
-=item o Other projects
-
-Many of these are discussed on Tamura's site.
-
-=over 4
-
-=item o L<http://bettergedcom.wikispaces.com/>
-
-=item o L<http://www.ngsgenealogy.org/cs/GenTech_Projects>
-
-=item o L<http://gdmxml.fugal.net/>
-
-=item o L<http://www.cosoft.org/genxml/>
-
-=item o L<http://www.sunflower.com/~billk/GEDC/>
-
-=item o L<http://ancestorsnow.blogspot.com/2011/07/vged.html>
-
-=item o L<http://www.tamurajones.net/GEDCOMValidation.xhtml>
-
-=item o L<http://webtrees.net/>
-
-=item o L<http://swoodbridge.com/Genealogy/lifelines/>
-
-=item o L<http://deadendssoftware.blogspot.com/>
-
-=item o L<http://www.legacyfamilytree.com/>
-
-=item o L<https://devnet.familysearch.org/docs/api-overview>
-
-=back
-
-=back
-
-=head1 The Gedcom Mailing List
-
-Contact perl-gedcom-help@perl.org.
-
 =head1 Support
 
 Email the author, or log a bug on RT:
 
 L<https://rt.cpan.org/Public/Dist/Display.html?Name=Genealogy::Gedcom>.
+
+=head1 Thanx
+
+Thanx to Eugene van der Pijll, the author of the Gedcom::Date::* modules.
 
 =head1 Author
 
