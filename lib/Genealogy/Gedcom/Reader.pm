694de1aa69abef74c510ed7e6570bf0aa4ccf53d
ronsavage <ron@savage.net.au>
2013-06-28 11:53:43 +1000

    Active debug flag in t/date.t trying to track test failures

--- lib/Genealogy/Gedcom/Reader.pm
+++ lib/Genealogy/Gedcom/Reader.pm
@@ -18,7 +18,7 @@ fieldhash my %minlevel     => 'minlevel';
 fieldhash my %report_items => 'report_items';
 fieldhash my %strict       => 'strict';
 
-our $VERSION = '0.83';
+our $VERSION = '0.84';
 
 # --------------------------------------------------
 
